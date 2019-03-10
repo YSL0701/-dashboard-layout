fbd5bdf9c0843f69fd3e6b7ae9c0e1e1cf8b37d1
YSL0701 <30719leo@gmail.com>
2019-03-11 01:55:43 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SideBarComponent } from './component/side-bar/side-bar.component';
+import { AdvancedSearchComponent } from './component/advanced-search/advanced-search.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    AdvancedSearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
